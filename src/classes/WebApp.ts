2494f52319c6fcc9b0b48d7f186dd754ef9b4b5e
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-06-24 11:49:48 +0700

    welcome to koa :)

---
+++ src/classes/WebApp.ts
@@ -0,0 +1,10 @@
+import {createServer} from "http";
+import * as Koa from "koa";
+import {all as koaRoute} from "koa-route";
+
+class WebApp extends Koa {
+  // TODO: extending Koa, add these methods:
+  // * addMiddleware(function)
+  // * addRoute(method, route, function)
+  // * createServer()
+}
