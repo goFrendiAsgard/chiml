846eedcd977c055db9f0e827bb5a650ddec33069
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-06-25 17:05:33 +0700

    fix up things

--- src/libraries/webMiddleware.ts
+++ src/libraries/webMiddleware.ts
@@ -1,8 +1,15 @@
-export function jsonRpc() {
-  // pass
+import * as koaRoute from "koa-route";
+
+export function jsonRpc(path): (...ins: any[]) => any {
+  return koaRoute.post(`${path}/:procedure`, (ctx, procedure) => {
+    // TODO: map a json rpc response
+    ctx.body = JSON.stringify({});
+  });
 }
 
-
-export function xmlRpc() {
-  // pass
+export function xmlRpc(path): (...ins: any[]) => any {
+  return koaRoute.post(`${path}/:procedure`, (ctx, procedure) => {
+    // TODO: map a xml rpc response
+    ctx.body = JSON.stringify({});
+  });
 }
