d4f1495e7d1c86883e928890e817b0e125b45636
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-11-20 14:30:46 +0700

    done

--- dist/chie.js
+++ dist/chie.js
@@ -2,31 +2,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
+const getArgs = require("get-args");
 if (require.main === module) {
     const rawArgs = process.argv.slice(2);
-    let injectionFile = null;
-    let yamlFile = null;
-    let args = [];
-    if (rawArgs.length === 0) {
-        console.error("Parameter expected");
+    const processedArgs = getArgs(rawArgs);
+    const args = processedArgs.arguments;
+    const { options } = processedArgs;
+    const injectionFile = options.i || options.injection || null;
+    const containerFile = options.c || options.container || null;
+    if (containerFile === null) {
+        console.error("Container expected");
     }
     else {
-        if (rawArgs.length === 1) {
-            // CASE: chie program.yaml
-            yamlFile = rawArgs[0];
-        }
-        else {
-            if (rawArgs[1].match(/.*\.yml$/gi)) {
-                // CASE: chie injection.js program.yaml arg1 arg2 ...argn
-                [injectionFile, yamlFile, ...args] = rawArgs;
-            }
-            else {
-                // CASE: chie program.yaml arg1 arg2 ...argn
-                [yamlFile, ...args] = rawArgs;
-            }
-        }
         // get bootstrap and run it
-        const bootstrap = index_1.execute(yamlFile, injectionFile);
+        const bootstrap = index_1.execute(containerFile, injectionFile);
         const result = bootstrap(...args);
         if ("then" in result) {
             result
--- example/test.sh
+++ example/test.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 tsc --build ${DIR}/tsconfig.json
-node ${DIR}/../dist/chie.js ${DIR}/animal-calendar.yml 2018
-node ${DIR}/../dist/chie.js ${DIR}/dist/dogInjection.js ${DIR}/animal-calendar.yml 2018
+node ${DIR}/../dist/chie.js -c ${DIR}/animal-calendar.yml 2018
+node ${DIR}/../dist/chie.js --injection ${DIR}/dist/dogInjection.js --container ${DIR}/animal-calendar.yml 2018
